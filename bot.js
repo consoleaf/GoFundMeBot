295fecb2a8bc1c699cbc76bca6d88c2d059bb33e
Aryslan Yakshibaev <yak.aryslan.1999@gmail.com>
2020-06-19 19:49:53 +0300

    yeet

--- bot.js
+++ bot.js
@@ -6,37 +6,72 @@ const wss = new WebSocket.Server({ port: 8080 });
 
 wss.on('connection', ws => {
     ws.on('message', message => {
-        console.log(`Received message: ${message}`);
+        console.log(`DEBUG: Received message: ${message}`);
     });
 });
 
 const project_id = 2319535;
 
-setInterval(() => {
-    wss.clients.forEach(ws => {
-        axios.get(`https://charity.gofundme.com/v2/project/${project_id}/donations`)
-            .then(resp => {
-                return resp.data;
-            })
-            .then(data => {
-                ws.send(JSON.stringify({
-                    type: 'donationsList',
-                    data: data.data
-                }));
-                axios.get(`https://charity.gofundme.com/v2/project/${project_id}`)
-                    .then(resp => {
-                        return resp.data;
-                    })
-                    .then(data => {
-                        ws.send(JSON.stringify({
-                            type: 'overview',
-                            total: data.data.project_total_display,
-                            goal: data.data.goal,
-                            currency: data.data.currency
-                        }));
-                    });
-            });
+const individual = true; // if individual
+// const individual = false; // if charity
 
-        console.log("Sent to client!");
-    });
-}, 5000);
+// This is the part of the link after the "/f/".
+const individual_project_id = "gu9tre-trying-to-escape";
+
+if (individual)
+    setInterval(() => {  // TODO: Remake this for individual version
+        wss.clients.forEach(ws => {
+            axios.get(`https://gateway.gofundme.com/web-gateway/v1/feed/${individual_project_id}/donations?sort=recent`)
+                .then(resp => {
+                    return resp.data;
+                })
+                .then(data => {
+                    ws.send(JSON.stringify({
+                        type: 'donationsList',
+                        data: data.data
+                    }));
+                    axios.get(`https://charity.gofundme.com/v2/project/${project_id}`)
+                        .then(resp => {
+                            return resp.data;
+                        })
+                        .then(data => {
+                            ws.send(JSON.stringify({
+                                type: 'overview',
+                                total: data.data.project_total_display,
+                                goal: data.data.goal,
+                                currency: data.data.currency
+                            }));
+                        });
+                });
+            console.log("DEBUG: Sent to client!");
+        });
+    }, 5000);
+else
+    setInterval(() => {
+        wss.clients.forEach(ws => {
+            axios.get(`https://charity.gofundme.com/v2/project/${project_id}/donations`)
+                .then(resp => {
+                    return resp.data;
+                })
+                .then(data => {
+                    ws.send(JSON.stringify({
+                        type: 'donationsList',
+                        data: data.data
+                    }));
+                    axios.get(`https://charity.gofundme.com/v2/project/${project_id}`)
+                        .then(resp => {
+                            return resp.data;
+                        })
+                        .then(data => {
+                            ws.send(JSON.stringify({
+                                type: 'overview',
+                                total: data.data.project_total_display,
+                                goal: data.data.goal,
+                                currency: data.data.currency
+                            }));
+                        });
+                });
+            console.log("DEBUG: Sent to client!");
+        });
+    }, 5000);
+
